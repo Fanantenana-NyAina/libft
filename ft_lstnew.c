b658c9b3f2531bae021769c436639941514ea7f5
Fanantenana Ny Aina Rakotoarison <fananrak@c1r5s2.42antananarivo.mg>
2026-02-11 14:11:14 +0300

    fix(ft_lstnew.c): norminatting

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fananrak <fananrak@student.42antananari    +#+  +:+       +#+        */
+/*   By: fananrak <fananrak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 15:06:43 by fananrak          #+#    #+#             */
-/*   Updated: 2026/02/11 13:51:37 by fananrak         ###   ########.fr       */
+/*   Updated: 2026/02/11 14:10:27 by fananrak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list	*ft_lstnew(void *content)
 	t_list	*node;
 
 	if (!content)
-		return (NULL);	
+		return (NULL);
 	node = malloc(sizeof(t_list));
 	if (!node)
 		return (0);
